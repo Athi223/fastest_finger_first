3b6d918962f7f044f9060b372818ac14bf36a08b
Atharv Phadnis <athiphadnis@gmail.com>
2023-02-04 15:37:55 +0530

    Added questions get and populate URLs

--- fastest_finger_first/competition/urls.py
+++ fastest_finger_first/competition/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("staff/", views.staff, name="staff"),
     path("signin/", views.signin, name="signin"),
     path("signout/", views.signout, name="signout"),
+    path("populate_questions/", views.populate_questions, name="populate_questions"),
+    path("get_question/", views.get_question, name="get_question"),
 ]
