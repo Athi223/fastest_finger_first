ac15e890089c4bc2a29beb41e7bacc8cd5166b09
Atharv Phadnis <athiphadnis@gmail.com>
2023-02-04 15:37:39 +0530

    Added Allotment and Question models

--- fastest_finger_first/competition/models.py
+++ fastest_finger_first/competition/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Allotment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    questions = models.TextField(default="[]")
+
+class Question(models.Model):
+    question = models.TextField()
+    option0 = models.TextField()
+    option1 = models.TextField()
+    option2 = models.TextField()
+    option3 = models.TextField()
+    correct = models.IntegerField()
