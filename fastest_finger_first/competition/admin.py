b3178cf73049f4a8b3ee680fa3461541560b7ec9
Atharv Phadnis <athiphadnis@gmail.com>
2023-02-04 15:36:51 +0530

    Added "Allotment" and "Question" models

--- fastest_finger_first/competition/admin.py
+++ fastest_finger_first/competition/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Allotment, Question
 
 # Register your models here.
+@admin.register(Allotment)
+class AllotmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
